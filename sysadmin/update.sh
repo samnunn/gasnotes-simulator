6408cfd0d403983effc3690c93c7878f4e6b2611
Sam Nunn <sam@nunn.io>
2024-08-04 14:50:16 +0800

    Remove redundant "down" command from update.sh

--- sysadmin/update.sh
+++ sysadmin/update.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker compose -f compose.base.yaml down
+# docker compose -f compose.base.yaml down
 git pull git@github.com:samnunn/gasnotes-simulator.git
 ./sysadmin/build.sh
 ./sysadmin/run.sh
