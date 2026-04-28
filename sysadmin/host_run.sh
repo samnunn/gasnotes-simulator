c48680af5f5114d3f9a540ea20a9b1434e8f6405
Sam Nunn <sam@nunn.io>
2026-04-28 15:17:20 +0800

    Fix host_run to reflect new venv location

--- sysadmin/host_run.sh
+++ sysadmin/host_run.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-(
-    cd ./app || exit 1
 
-    export RATELIMIT_ENABLED=0
-    export SIM_ROOM_STORE=sim_rooms
-    export SECRET_KEY=dummy_secret_key
+export RATELIMIT_ENABLED=0
+export SIM_ROOM_STORE=sim_rooms
+export SECRET_KEY=dummy_secret_key
 
-    uv run flask --app app.wsgi:app --debug run --host=0.0.0.0 --port=8069
-)
+uv run flask --app app.wsgi:app --debug run --host=0.0.0.0 --port=8069
